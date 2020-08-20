491cec9af7d5a735b1c47a133d5abc5df8c5183f
wooriml <31747242+wooriml@users.noreply.github.com>
2020-08-20 13:20:20 +0900

    Website first commit

---
+++ clean.sh
@@ -0,0 +1,3 @@
+rm -r bootstrap css fonts img index.html js LineaIcons 
+rm mail.php welcome.tar clean.sh
+tar xvf welcome.tar
---
+++ make.sh
@@ -0,0 +1,2 @@
+tar cvf welcome.tar bootstrap css fonts img js LineaIcons index.html mail.php
+scp welcome.tar wooriml@shell.uoregon.edu:public_html/
