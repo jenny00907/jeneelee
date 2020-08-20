4dcf1cfd3f94435e7fd5b5030d4c20b9763cf864
Jenny Woorim Lee <jennylee@jennylees-MacBook-Pro.local>
2020-08-20 13:37:19 +0900

    Deleting old files

--- clean.sh
+++
@@ -1,3 +0,0 @@
-rm -r bootstrap css fonts img index.html js LineaIcons 
-rm mail.php welcome.tar clean.sh
-tar xvf welcome.tar
--- make.sh
+++
@@ -1,2 +0,0 @@
-tar cvf welcome.tar bootstrap css fonts img js LineaIcons index.html mail.php
-scp welcome.tar wooriml@shell.uoregon.edu:public_html/
